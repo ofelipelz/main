41ce56ee50f12b44557e2d4da5dbef0621fe494c
ofelipelz <126736141+ofelipelz@users.noreply.github.com>
2025-10-21 23:59:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pagina-de-direcionamento
+
---
+++ script.js
@@ -0,0 +1,41 @@
+ class BarraDeNavegacao{
+    constructor(menu, navList, navLinks){
+        this.menu = document.querySelector(menu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+        
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+                link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.menu.classList.toggle(this.activeClass)
+        this.animateLinks()
+    }
+
+    addClickEvent(){
+        this.menu.addEventListener("click", this.handleClick);
+    }
+
+    init(){
+        if(this.menu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+ }
+
+ const barraDeNavegacao = new BarraDeNavegacao(
+    ".menu",
+    ".nav-list",
+    ".nav-list li",
+ );
+ barraDeNavegacao.init();
